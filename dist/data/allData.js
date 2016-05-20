771cbf1fdb6f0edeba46f2ca063bfcc762faf300
Marcus Tran <marcus@hoiio.com>
2016-05-20 13:03:03 +0700

    v2.0

--- dist/data/allData.js
+++ dist/data/allData.js
@@ -90,38 +90,61 @@ d3.csv("/standard-wip/dist/data/menu.csv", function (error, data) {
 
     });
 });
-/*
-d3.csv("/standard-wip/dist/pattern/pattern.csv", function (error, data) {
+
+d3.csv("/standard-wip/dist/data/content.csv", function (error, data) {
+
+    data - link = "pattern"
     var content = d3.select("#content");
     data.forEach(function (d) {
 
-        if (d.type == "pattern") {
-            var block = content.append("div")
-                .attr("class", "block")
-                .attr("id", d.id);
-            block.append("div")
-                .attr("class", "title")
-                .attr("data-align", "center")
+
+        if (d.type == "block") {
+            var block = d3.select("ul#" + d.id);
+            var li = block.append("li")
+                .attr("class", "item")
+                .attr("data-link", d.link);
+            var ul = li.append("ul")
+                .attr("id", d.link);
+            ul.append("li")
+                .attr("class", "item header")
                 .text(d.title);
-            block.append("div")
-                .attr("class", "ui doubling stackable four column grid")
-                .attr("data-align", "left");
         }
 
-        if (d.type == "child") {
-            var grid = d3.select("#" + d.id + " .grid");
-            var column = grid.append("div").attr("class", "column");
-            var item = column.append("div").attr("class", "item");
-
-            item.append("div").attr("class", "title").text(d.title);
-            var thumbnail = item.append("div").attr("class", "thumbnail");
-            thumbnail.append("img").attr("class", "ui image").attr("src", "dist/img/pattern/" + d.id + "/" + d.img);
-
-            item.append("p").attr("class", "description").html(d.description);
-
+        if (d.type == "item") {
+            var block = d3.select("ul#" + d.id);
+            var li = block.append("li")
+                .attr("class", "item")
+                .attr("data-pattern", d.link);
+            li.append("a")
+                .text(d.title);
         }
 
-
+//
+//        if (d.type == "pattern") {
+//            var block = content.append("div")
+//                .attr("class", "block")
+//                .attr("id", d.id);
+//            block.append("div")
+//                .attr("class", "title")
+//                .attr("data-align", "center")
+//                .text(d.title);
+//            block.append("div")
+//                .attr("class", "ui doubling stackable four column grid")
+//                .attr("data-align", "left");
+//        }
+//
+//        if (d.type == "child") {
+//            var grid = d3.select("#" + d.id + " .grid");
+//            var column = grid.append("div").attr("class", "column");
+//            var item = column.append("div").attr("class", "item");
+//
+//            item.append("div").attr("class", "title").text(d.title);
+//            var thumbnail = item.append("div").attr("class", "thumbnail");
+//            thumbnail.append("img").attr("class", "ui image").attr("src", "dist/img/pattern/" + d.id + "/" + d.img);
+//
+//            item.append("p").attr("class", "description").html(d.description);
+//
+//        }
+//
     });
 })
-*/
