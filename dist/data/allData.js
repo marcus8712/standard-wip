49c54cf018b378f997cb5d777031ba852b67ccc8
Marcus Tran <marcus@hoiio.com>
2016-05-20 13:03:50 +0700

    v2.0

--- dist/data/allData.js
+++ dist/data/allData.js
@@ -93,10 +93,7 @@ d3.csv("/standard-wip/dist/data/menu.csv", function (error, data) {
 
 d3.csv("/standard-wip/dist/data/content.csv", function (error, data) {
 
-    data - link = "pattern"
-    var content = d3.select("#content");
     data.forEach(function (d) {
-
 
         if (d.type == "block") {
             var block = d3.select("ul#" + d.id);
@@ -119,32 +116,32 @@ d3.csv("/standard-wip/dist/data/content.csv", function (error, data) {
                 .text(d.title);
         }
 
-//
-//        if (d.type == "pattern") {
-//            var block = content.append("div")
-//                .attr("class", "block")
-//                .attr("id", d.id);
-//            block.append("div")
-//                .attr("class", "title")
-//                .attr("data-align", "center")
-//                .text(d.title);
-//            block.append("div")
-//                .attr("class", "ui doubling stackable four column grid")
-//                .attr("data-align", "left");
-//        }
-//
-//        if (d.type == "child") {
-//            var grid = d3.select("#" + d.id + " .grid");
-//            var column = grid.append("div").attr("class", "column");
-//            var item = column.append("div").attr("class", "item");
-//
-//            item.append("div").attr("class", "title").text(d.title);
-//            var thumbnail = item.append("div").attr("class", "thumbnail");
-//            thumbnail.append("img").attr("class", "ui image").attr("src", "dist/img/pattern/" + d.id + "/" + d.img);
-//
-//            item.append("p").attr("class", "description").html(d.description);
-//
-//        }
-//
+        //
+        //        if (d.type == "pattern") {
+        //            var block = content.append("div")
+        //                .attr("class", "block")
+        //                .attr("id", d.id);
+        //            block.append("div")
+        //                .attr("class", "title")
+        //                .attr("data-align", "center")
+        //                .text(d.title);
+        //            block.append("div")
+        //                .attr("class", "ui doubling stackable four column grid")
+        //                .attr("data-align", "left");
+        //        }
+        //
+        //        if (d.type == "child") {
+        //            var grid = d3.select("#" + d.id + " .grid");
+        //            var column = grid.append("div").attr("class", "column");
+        //            var item = column.append("div").attr("class", "item");
+        //
+        //            item.append("div").attr("class", "title").text(d.title);
+        //            var thumbnail = item.append("div").attr("class", "thumbnail");
+        //            thumbnail.append("img").attr("class", "ui image").attr("src", "dist/img/pattern/" + d.id + "/" + d.img);
+        //
+        //            item.append("p").attr("class", "description").html(d.description);
+        //
+        //        }
+        //
     });
 })
